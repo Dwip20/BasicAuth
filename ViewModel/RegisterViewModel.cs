54ffb5e2dbc9e48921ff8603765cc9ecbc0830b0
Dwip Sasmal <dwip.sasmal@gmail.com>
2026-02-25 00:47:23 +0530

    Establish DB Connection

---
+++ ViewModel/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicAuth.ViewModel
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }  
+        public string ContactNumber { get; set; }
+    }
+}
