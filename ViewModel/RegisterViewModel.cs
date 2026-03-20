b1ab46cd5bcd3765cb9a50f6608a197c1e4ad46a
Dwip Sasmal <dwip.sasmal@gmail.com>
2026-03-20 23:47:45 +0530

    Add New Project

--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -6,7 +6,6 @@ namespace BasicAuth.ViewModel
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public string PhoneNo { get; set; }
-        public string Password { get; set; }
+       
     }
 }
